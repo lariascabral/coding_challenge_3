4b97a854ac6c0025dc2898b0e93ddda488ddd803
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-02-02 12:27:35 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,7 @@
+// Task 1 - Product Price Management
+
+let prices = [50.00, 19.99, 23.95, 79.95, 17.99, 38.95] ;     // Creation of prices' array
+prices[6] = 45.99 ;                                           // Adding a new element to prices' array
+prices.shift() ;                                              // Correcting and eliminating error in typing for the array
+
+console.log("Prices:", prices)                              // Output: Prices: 19.99, 23.95, 79.95, 17.99, 38.95, 45.99
