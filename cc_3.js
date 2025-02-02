c1e98a3fa1946d7430495bb2293c09f4e8342787
Lisbeth Arias Cabral <lariascabral@usf.edu>
2025-02-02 12:58:34 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -1,7 +1,17 @@
 // Task 1 - Product Price Management
 
-let prices = [50.00, 19.99, 23.95, 79.95, 17.99, 38.95] ;     // Creation of prices' array
+let prices = [17.99, 19.99, 23.95, 79.95, 17.99, 38.95] ;     // Creation of prices' array
 prices[6] = 45.99 ;                                           // Adding a new element to prices' array
-prices.shift() ;                                              // Correcting and eliminating error in typing for the array
+prices.shift() ;                                              // Eliminating a duplicated price
 
-console.log("Prices:", prices)                              // Output: Prices: 19.99, 23.95, 79.95, 17.99, 38.95, 45.99
+console.log("Prices:", prices)                                // Output: Prices: 19.99, 23.95, 79.95, 17.99, 38.95, 45.99
+
+
+// Task 2 - Modifying Customer Orders
+
+let orders = [29, 50, 30, 61, 44, 12]                         // Declaration of the array
+orders[2] += 5                                                // Incrasing order quantity by 5
+let totalOrders = orders.reduce((sum, quantity) => sum + quantity, 0)       // Calculation of total orders
+
+console.log("Updated Orders Quantities:", orders)            // Output: Updated Orders Quantities: (6) [29, 50, 35, 61, 44, 12]
+console.log("Total Orders (count):", totalOrders)            // Output: Total Orders (count): 231
